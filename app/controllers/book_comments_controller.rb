86ca743a5b477cdb121f361537814eab6750c056
kasu1104kasu <kasumikun1104@gmail.com>
2021-09-30 17:51:57 +0000

    first commit

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,28 @@
+class BookCommentsController < ApplicationController
+
+  def create
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
+      redirect_to book_path(@book)
+    else
+      @bookadd = Book.new
+      @user = @book.user
+      render 'books/show'
+    end
+  end
+
+  def destroy
+    comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    comment.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+
+end
